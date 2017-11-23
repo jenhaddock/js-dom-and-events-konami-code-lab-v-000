11591fe811ab7e2b18ea10ef3674c1efd12f799d
Jennifer Haddock <jentx123@hotmail.com>
2017-11-23 00:28:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,18 +5,18 @@ let index = 0;
 function init(e) {
   // Write your JavaScript code inside the init() function
   const key = parseInt(e.detail || e.which);
- 
+
   if (key === code[index]) {
     index++;
- 
+
     if (index === code.length) {
       alert("Congrats!");
- 
+
       index = 0;
     }
   } else {
     index = 0;
   }
-    
+
   }
 }
