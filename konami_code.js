ccbf2931432451852919e2037aa60bd26b6b2914
Jennifer Haddock <jentx123@hotmail.com>
2017-11-23 00:37:08 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,10 +5,10 @@ let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', keyDown);
-  
+
   function keyDown (e){
      const key = parseInt(e.detail || e.which);
-     
+
      if (key === code[index]) {
       index++;
 
